1cbd5b56da90240afe8a460b4720a395c190ee41
andruAJ <giraldoandresjuan0405@gmail.com>
2025-05-14 15:42:04 -0500

    NamesPanel ya funcional
    solo falta que muestre para las bandas

--- H.E.R.M.E.S/Assets/Scripts/HoverManager.cs
+++ H.E.R.M.E.S/Assets/Scripts/HoverManager.cs
@@ -1,10 +1,20 @@
+using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class HoverManager : MonoBehaviour
 {
     public UIDocument document;
-    private TextField namePanel;
+    private VisualElement namePanel;
+    public CapasInformation panelSupIzq;
+    public CapasInformation panelInfIzq;
+    public CapasInformation panelSupDer;
+    public CapasInformation panelInfDer;
+    public CapasInformation asientos;
+    public CapasInformation bandaTransVert;
+    public CapasInformation bandaTransHoriz;
+    public CapasInformation intercambiador;
 
     void Start()
     {
@@ -14,7 +24,7 @@ public class HoverManager : MonoBehaviour
             Debug.LogError("No se encontró el UIdocument");
             return;
         }
-        namePanel = document.rootVisualElement.Q("NamePanel") as TextField;
+        namePanel = document.rootVisualElement.Q("NamesPanel") as VisualElement;
         if(namePanel != null)
         {
             Debug.Log("NamePanel no encontrado");
@@ -22,10 +32,45 @@ public class HoverManager : MonoBehaviour
     }
     private void OnMouseEnter()
     {
-        Debug.Log(this.tag);
+        switch(this.tag) 
+        {
+            case "Asientos":
+                namePanel.style.display = DisplayStyle.Flex;
+                namePanel.Bind(new SerializedObject(asientos));       
+                break;
+            case "Panel superior":
+                Debug.Log("Data: " + panelSupIzq);
+                namePanel.style.display = DisplayStyle.Flex;
+                namePanel.Bind(new SerializedObject(panelSupIzq));               
+                break;
+            case "Panel inferior":
+                namePanel.style.display = DisplayStyle.Flex;
+                namePanel.Bind(new SerializedObject(panelInfIzq));             
+                break;
+            case "Panell superior derecho":
+                namePanel.style.display = DisplayStyle.Flex;
+                namePanel.Bind(new SerializedObject(panelSupDer));               
+                break;
+            case "Panell inferior derecho":
+                namePanel.style.display = DisplayStyle.Flex;
+                namePanel.Bind(new SerializedObject(panelInfDer));  
+                break;
+            case "Banda transportadora vertical":
+                namePanel.style.display = DisplayStyle.Flex;
+                namePanel.Bind(new SerializedObject(bandaTransVert));
+                break;
+            case "Banda transportadora horizontal":
+                namePanel.style.display = DisplayStyle.Flex;
+                namePanel.Bind(new SerializedObject(bandaTransHoriz));   
+                break;
+            case "Intercambiador":
+                namePanel.style.display = DisplayStyle.Flex;
+                namePanel.Bind(new SerializedObject(intercambiador));     
+                break;
+        }
     }
     private void OnMouseExit() 
     {
-    
+        namePanel.style.display = DisplayStyle.None;
     }
 }
